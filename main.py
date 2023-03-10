ac3b8f0d6067ac8fdeccffc5310166c0eddbe304
GabrielCASilva <gabriel.foxcarvalho@gmail.com>
2023-03-10 08:59:09 -0300

    creating an post with BaseModel

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
-from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    # to validate payload post content
+    title: str
+    category: str
+    content: str
+    author: str
+    published: bool = True  # optional field with default value
+    rating: Optional[int] = None
 
 
 @app.get("/")
@@ -11,10 +23,10 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is yout posts"}
+    return {"data": "This is your posts"}
 
 
 @app.post("/posts")
-def post_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f"title {payload['title']} content: {payload['content']}"}
+def post_posts(payload: Post):
+    post = payload.dict()
+    return {"data": post, "message": "successfully create a new post"}
