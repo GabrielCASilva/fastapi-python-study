120a8f4278dd43746bcba062ca2da2a3376c4e86
GabrielCASilva <gabriel.foxcarvalho@gmail.com>
2023-03-12 12:20:10 -0300

    make patch request

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
 from fastapi import FastAPI, Response, status, HTTPException
-from pydantic import BaseModel
-from typing import Optional
+from schemas import Post, PatchPost
 from random import randrange
 from datetime import date
 
 
 app = FastAPI()
-
-
-class Post(BaseModel):
-    # to validate payload post content
-    id: int = 0
-    title: str
-    category: str
-    content: str
-    author: str
-    published: bool = True  # optional field with default value
-    rating: Optional[int] = None
-    created_at: str = "0"
 
 
 my_posts = []
@@ -27,6 +14,12 @@ def find_post(id):
     for post in my_posts:
         if post["id"] == id:
             return post
+
+
+def find_index_post(id):
+    for i, post in enumerate(my_posts):
+        if post["id"] == id:
+            return i
 
 
 @app.get("/")
@@ -62,3 +55,38 @@ def post_posts(payload: Post):
     my_posts.append(post_dict)
 
     return {"data": post_dict}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    post = find_post(int(id))
+
+    if post == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id: {id} was not found",
+        )
+
+    my_posts.remove(post)
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.patch("/posts/{id}")
+def patch_post(id: int, payload: PatchPost):
+    post = payload.dict()
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id: {id} was not found",
+        )
+
+    for key, value in post.items():
+        if value is not None:
+            my_posts[index][key] = value
+
+    my_posts[index]["update_at"] = date.today().strftime("%d/%m/%Y")
+
+    return {"message": "post was successfully patched"}
---
+++ schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class Post(BaseModel):
+    # to validate payload post content
+    id: Optional[int] = None
+    title: str
+    category: str
+    content: str
+    author: str
+    published: bool = True  # optional field with default value
+    rating: Optional[int] = None
+    created_at: Optional[int] = None
+
+
+class PatchPost(Post):
+    title: Optional[str] = None
+    category: Optional[str] = None
+    content: Optional[str] = None
+    author: Optional[str] = None
