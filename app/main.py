822f04d075a93bc97596a482afe36b43ec34cbc2
GabrielCASilva <gabriel.foxcarvalho@gmail.com>
2023-03-29 09:01:55 -0300

    ending database models config

--- app/main.py
+++ app/main.py
@@ -5,8 +5,21 @@ from datetime import date
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from . import models
+from .database import engine, SessionLocal
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 my_posts = []
 
--- app/models.py
+++ app/models.py
@@ -1,8 +1,33 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 from .database import Base
 
-# post table
+
 class Post(Base):
     __tablename__ = "posts"
 
     id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    published = Column(Boolean, default=True)
+    # created_at = Column()
+    # upddated_at = Column()
+    id_user = Column(Integer, ForeignKey("user.id"), nullable=False)
+    user = relationship("user")
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+
+
+class Likes(Base):
+    __tablename__ = "likes"
+
+    id_user = Column(Integer, ForeignKey("user.id"), primary_key=True)
+    id_post = Column(Integer, ForeignKey("posts.id"), primary_key=True)
+    user = relationship("users")
+    post = relationship("posts")
